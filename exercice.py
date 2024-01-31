fa610fe94c1ff4481659a9d421ccff57b5ecfca0
davidgikungu <157691878+davidgikungu@users.noreply.github.com>
2024-01-31 13:25:01 +0300

    Add files via upload

---
+++ exercice.py
@@ -0,0 +1,8 @@
+
+#enter value in degree celcius
+def calculate():
+    celcius= eval(input("enter temperature in degrees celcius"))
+    fahrehiet = (9/5) * celcius + 32
+    print("temperature is",fahrehiet)
+calculate()
+ 
